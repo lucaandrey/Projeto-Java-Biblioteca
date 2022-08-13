89d8a679aa080395d5ce0181985c0b5a9f317e51
Luca <lucaandrey5@hotmail.com>
2022-08-13 18:42:14 -0300

    Corringindo bugs

--- Main.java
+++ Main.java
@@ -6,16 +6,13 @@ public class Main {
         Scanner input = new Scanner(System.in);
         English c1 = new English("asd","asd","asd",0,0,0);
 
-
         Biblioteca c2 = new Biblioteca("asd","asd","asd",0,0,0);
-        System.out.println("Choose a language|| Escolha um idioma: ");
-        System.out.println(" ");
-        System.out.println("(1)  English     ||    (2) Portugues");
 
         Espanol c3 = new Espanol("asd","asd","asd",0,0,1);
+       
         System.out.println("Choose a language|| Escolha um idioma: ");
         System.out.println(" ");
-        System.out.println("(1)  English     ||   1(2) Portugues	||	(3) Espa�ol		");
+        System.out.println("(1)  English     ||   (2) Portugues	||	(3) Espa�ol		");
 
         a = input.nextInt();
         if(a  == 1){
@@ -23,8 +20,6 @@ public class Main {
         }
         else if(a == 2){
             c2.cadastro();
-            
-
         }
         else if(a == 3) {
         	c3.cadastro();
